99c40e25ad358cab74cc844794722f885bf9f76b
Mariia Gushuley <maryshka.moroz@gmail.com>
2022-07-20 11:55:56 +0200

    added examples

--- index.js
+++ index.js
@@ -1,7 +1,43 @@
-'use strict';
+"use strict";
 /*
  *  Масиви: літерал масива, елементи, індексація, довжина
  */
+
+// const str = 'abs';// 'abs'= 'abs'
+// const str2 = 'abs';
+// const str3 = "abs";
+// console.log(str===str3)
+
+// const a = ['Bob', 'Alice', 'Traice']; //[3]===[3] [], link1
+// const b = a; // link2
+// // console.log(b === a);
+// console.log(a);
+// a.push('Sara');
+// console.log(a);
+// b.push('Ted', 'Ella', "Jonh");
+// console.log(a)
+// console.log(b)
+//  const arr1 = [1, 2, 3];
+//  const arr2 = [1, 2, 3];
+//  console.log(arr1 === arr2);
+
+// const fruit = "apple";
+
+// if (fruit === "apple" || fruit === "strawberry") {
+//   console.log("It is a red fruit!");
+// }
+
+// const someArr = [];
+// const arr  = [3, 4, null, 7];
+// // arr = [2,4,6];
+// console.log(arr);
+// console.log(arr.length);
+// arr[2]= 5;
+// console.log(arr[2]);
+// // console.log(arr[13])
+// arr[45]=89;
+// console.log(arr);
+// console.log(arr[11]);
 
 /*
 ? Створіть масив genres з елементами Jazz і Blues.
@@ -12,6 +48,18 @@
 ? Вставте «Country» та «Reggy» на початок масиву.
  */
 
+// const genres = ['Jazz', 'Blues'];
+// // genres[2]='Rock&Roll';
+// genres.push('Rock&Roll');
+// console.log(genres[0]);
+// const lastIndex = genres.length-1;
+// console.log(genres[lastIndex]);
+// // genres.shift();
+// const firstElement = genres.shift();
+// console.log(genres);
+// console.log(firstElement);
+// genres.unshift('Country', 'Reggy');
+// console.log(genres);
 
 /*
  * Присвоєння за посиланням і за значенням
@@ -24,6 +72,13 @@
 ? Змініть значення змінної a і ще раз виведіть у консоль
 */
 
+// let a = 10; //
+// let b = 10; //10
+// console.log(a);
+// console.log(b);
+// a=20;
+// console.log(a);
+// console.log(b);
 
 /*
 ? Створіть масив arr1 = [1, 2, 3].
@@ -32,7 +87,13 @@
 ? Додайте елемент в масив arr1 і ще раз виведете обидва масиви в консоль
 */
 
-
+// const arr1 = [1, 2, 3]; //link1
+// const arr2 = [1, 2, 3]; //link2
+// console.log(arr1);
+// console.log(arr2);
+// arr1.push(4,5);
+// console.log(arr1);
+// console.log(arr2);
 
 /*
  * Перебор масива циклами for та for...of
@@ -46,6 +107,14 @@
 
 // const fruits = ['🍎', '🍇', '🍑', '🍌', '🍋'];
 
+// for( let i=0; i < fruits.length; i+=1) {
+//   console.log(`${i+1}: ${fruits[i]}`)
+// }
+
+// for(const fruit of fruits) {
+//   const index = fruits.indexOf(fruit);
+//   console.log(`${index+1}: ${fruit}`);
+// }
 
 /*
 ? Напиши скрипт пошуку найменшого числа у масиві.
@@ -53,6 +122,26 @@
 ? Використовуйте цикл для вирішення задачі.
 */
 
+// const numbers = [2, 17, 94, 1, -20, -1, -23, 23, 40, 35, -50];
+// let min = "чисел не знайдено";
+// if (numbers.length) {
+//   min = 0;
+//   for (let i = 0; i < numbers.length; i += 1) {
+
+//     if(numbers[i] < min) {
+//       min = numbers[i];
+//     }
+// }
+// let min = numbers[0];
+
+
+// for (const item of numbers) {
+//   if (item < min) {
+//     min = item;
+//   }
+// }
+// // }
+// console.log(min);
 
 /*
 ? У нас є масив із зарплатами працівників, потрібно порахувати загальну суму зарплат
@@ -60,7 +149,31 @@
 
 // const salaries = [200, 450, 600, 150, 300];
 
+// let total = 0;
 
+// for (let i =0; i < salaries.length; i+=1){
+//   total +=salaries[i];
+// }
+
+// for( const salary of salaries) {
+//   total += salary;
+// }
+
+// console.log(total);
+// const salaries = [200, 450, 600, 150, 300];
+// let item = 0
+// while(item < salaries.length){
+//  total+= salaries[item];
+//  item +=1;
+// }
+
+// console.log(total);
+
+// do {
+//   total+= salaries[item];
+//   item +=1;
+// } while (item < salaries.length);
+// console.log(total);
 /*
 ? У нас є кілька масивів із зарплатами працівників із різних відділів, потрібно порахувати загальну суму зарплат
 */
@@ -68,7 +181,19 @@
 // const managerSalaries = [100, 150, 250, 400, 500];
 // const developersSalaries = [800, 1500, 4000];
 
+// let total = 0;
 
+// const allSalaries = managerSalaries.concat(developersSalaries);
+// for (const salary of allSalaries) {
+//   if(salary > 1000) {
+//     const newSalary = salary + 200;
+//     total += newSalary
+//   } else {
+//     total += salary;
+//   }
+  
+// }
+// console.log(total);
 
 /*
  * Базові методи: split та join, indexOf та includes, push, slice та splice, concat
@@ -79,9 +204,16 @@
 ? значення яких зберігаються в змінній values у вигляді рядка.
 ? Значення гарантовано розділені пробілом.
 */
-
+// const newValue = [];
 // const values = '8 11';
+// const newValue = [];
+// newValue.push(values);
+// console.log(newValue)
+// const sidesArr = values.split(" ");
+// console.log(sidesArr);
 
+// const result = Number.parseInt(sidesArr[0]) * Number.parseInt(sidesArr[1]);
+// console.log(result);
 
 /*
 ? Напиши скрипт, який виводить в консоль ім'я та телефонний номер користувача.
@@ -93,6 +225,14 @@
 // const names = 'Jacob,William,Solomon,Artemis';
 // const phones = '89001234567,89001112233,890055566377,890055566300';
 
+// const nameArr = names.split(',');
+// const phonesArr = phones.split(',');
+// console.log(nameArr);
+// console.log(phonesArr);
+
+// for( let i=0; i < nameArr.length; i +=1) {
+//   console.log(`${i+1}: ${nameArr[i]} - ${phonesArr[i]}`)
+// }
 
 
 /*
@@ -101,9 +241,24 @@
 */
 // const string = 'Welcome to the future';
 
+// const charsArr = string.split('');
+// console.log(charsArr)
+// const reversCharsArr = [];
 
+// for (let i=charsArr.length-1; i>=0; i -=1){
+//   reversCharsArr.push(charsArr[i]);
+// }
+// console.log(reversCharsArr);
 
+// const revertStr = reversCharsArr.join('');
+// console.log(revertStr);
+// const charsArr = string.split('');
+// const reversChar = charsArr.reverse();
+// const resultString = reversChar.join('');
+// console.log(resultString);
 
+// const charsArr = string.split('').reverse().join('');
+// console.log(charsArr);
 /*
 ? У нас є масив співробітників, відсортуйте його, щоб співробітники не повторювалися
 */
@@ -128,18 +283,11 @@
 6 === 3
 */
 
-
-
 //? Співробітник Shaw звільнився, видаліть його з масиву
-
 
 //? Додати нового співробітника Thornton, перед Watkins;
 
-
-
-
-
- // ## Example 5 - Массивы и строки
+// ## Example 5 - Массивы и строки
 
 // // Напиши скрипт который выводит в консоль все слова строки кроме первого и
 // // последнего. Результирующая строка не должна начинаться или заканчиваться
@@ -147,7 +295,6 @@
 
 // // ```js
 // // const string = "Welcome to the future";
-
 
 // // ```
 
@@ -179,4 +326,3 @@
 // // let min;
 // // console.log(min); // 1
 // // ```
-
